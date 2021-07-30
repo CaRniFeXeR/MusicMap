548108de69ec19ba00cbcb359a10ef146d44bc64
Florian Kowarsch <florian.kowarsch@tuwien.ac.at>
2021-07-30 14:13:00 +0200

    init d3 js

--- frontend/scripts/queries.js
+++ frontend/scripts/queries.js
@@ -1,15 +1,17 @@
 function runMapQuery(path) {
-    fetch(`${window.origin}/` + path, {
-        method: "GET",
-        cache: "no-cache",
-        headers: new Headers({
-            "content-type": "application/json"
-        })
-    }).then(response => response.json())
-        .then(data => {
-            console.log(data)
-        })
-        .catch((error) => {
-            console.error('Error:', error);
-        });
-}
+    return fetch(`${window.origin}/` + path, {
+            method: "GET",
+            cache: "no-cache",
+            headers: new Headers({
+                "content-type": "application/json"
+            })
+        }).then(response => response.json())
+            .then(data => {
+                console.log("recived data")
+                data_parsed = Object.values(JSON.parse(data))
+                return data_parsed
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+    }
