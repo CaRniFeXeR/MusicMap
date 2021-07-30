046582930f3655ba8e6e01bdc9388dd876680e10
Florian Kowarsch <florian.kowarsch@tuwien.ac.at>
2021-07-30 13:37:35 +0200

    restructered umap embedding code

---
+++ backend/songembedding.py
@@ -0,0 +1,31 @@
+from typing import Union
+import pandas as pd
+from pandas.core.frame import DataFrame
+from sklearn import preprocessing
+import umap
+import umap.plot
+
+
+def embedd_csv(csv_path: str) -> Union[pd.DataFrame, Union[umap.UMAP, pd.DataFrame]]:
+    song_data = pd.read_csv(csv_path, index_col=0)
+    return song_data, embedd_data(song_data)
+
+
+def embedd_data(song_data: pd.DataFrame) -> Union[umap.UMAP, pd.DataFrame]:
+
+    song_data_scaled = preprocessing.StandardScaler().fit_transform(song_data.loc[:, song_data.columns != "name"])
+    mapper = umap.UMAP().fit(song_data_scaled)
+    song_data_transformed = mapper.transform(song_data_scaled)
+
+    return mapper, song_data_transformed
+
+
+def visualize_embedding(mapper: umap.UMAP, hover_df: pd.DataFrame):
+    p = umap.plot.interactive(mapper, hover_data=hover_df, point_size=5)
+    umap.plot.show(p)
+
+
+if __name__ == "__main__":
+    data_df, (mapper, data_embedded) = embedd_csv("playlists.csv")
+
+    visualize_embedding(mapper, data_df)
