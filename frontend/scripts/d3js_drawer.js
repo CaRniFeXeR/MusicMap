9672427bde976983fc10ac29f7509210a15e38ff
Florian Kowarsch <florian.kowarsch@tuwien.ac.at>
2021-08-02 22:40:26 +0200

    refactored d3js --> seperated initailization and drawing

--- frontend/scripts/d3js_drawer.js
+++ frontend/scripts/d3js_drawer.js
@@ -1,3 +1,5 @@
+var svg, xAxis, yAxis, x, y, scatter = "";
+
 
 function handleZoom() {
     console.log("handle zoom")
@@ -12,19 +14,17 @@ function initZoom() {
         .call(zoom);
 }
 
-
-function plot_data(data) {
-
+function init_d3_svg() {
 
     // set the dimensions and margins of the graph
-    var margin = { top: 10, right: 30, bottom: 30, left: 60 },
+    margin = { top: 10, right: 30, bottom: 30, left: 60 },
         width = "95vw",
         height = "80vh";
     client_width = document.documentElement.clientWidth;
     client_height = document.documentElement.clientHeight;
 
     // append the svg object to the body of the page
-    var svg = d3.select("#my_dataviz")
+    svg = d3.select("#my_dataviz")
         .append("svg")
         .attr("width", width)
         .attr("height", height)
@@ -55,23 +55,66 @@ function plot_data(data) {
     // initZoom()
 
     // Add X axis
-    var x = d3.scaleLinear()
+    x = d3.scaleLinear()
         .domain([4, 8])
         .range([0, client_width]);
-    var xAxis = svg.append("g")
+    xAxis = svg.append("g")
         .attr("transform", "translate(0," + client_height + ")")
         .call(d3.axisBottom(x));
 
     // Add Y axis
-    var y = d3.scaleLinear()
+    y = d3.scaleLinear()
         .domain([0, 9])
         .range([client_height, 0]);
-    var yAxis = svg.append("g")
+    yAxis = svg.append("g")
         .call(d3.axisLeft(y));
 
-    // Add dots
-    var scatter = svg.append('g')
-    // .attr("clip-path", "url(#clip)")
+    scatter = svg.append('g')
+
+}
+
+// A function that updates the chart when the user zoom and thus new boundaries are available
+function updateChart() {
+
+    current_zoom_level = d3.event.transform.k
+    console.log("zoom factor " + current_zoom_level)
+
+    let display_text = current_zoom_level <= 2.5 ? "none" : "block"
+
+    // recover the new scale
+    var newX = d3.event.transform.rescaleX(x);
+    var newY = d3.event.transform.rescaleY(y);
+
+    // update axes with these new boundaries
+    xAxis.call(d3.axisBottom(newX))
+    yAxis.call(d3.axisLeft(newY))
+
+    // update circle position
+    scatter
+        .selectAll("circle")
+        .attr('cx', function (d) { return newX(d["0"]) })
+        .attr('cy', function (d) { return newY(d["1"]) });
+
+    scatter
+        .selectAll("text")
+        .attr('x', function (d) { return newX(d["0"]) })
+        .attr('y', function (d) { return newY(d["1"]) })
+        .style("display", display_text);
+
+
+}
+
+
+function plot_songs(data) {
+
+}
+
+function plot_playlists() {
+
+}
+
+function plot_data(data) {
+
 
     scatter_data = scatter
         .selectAll("circle")
@@ -106,50 +149,18 @@ function plot_data(data) {
         })
         .style("font-size", "14px");
 
+}
 
+//transitions
 
-    // A function that updates the chart when the user zoom and thus new boundaries are available
-    function updateChart() {
+function handleMouseOver(d, i) {
+    d3.select(this).transition()
+        .duration(1)
+        .attr("r", 12);
+}
 
-        current_zoom_level = d3.event.transform.k
-        console.log("zoom factor " + current_zoom_level)
-
-        let display_text = current_zoom_level <= 1.5 ? "none" : "block"
-
-        // recover the new scale
-        var newX = d3.event.transform.rescaleX(x);
-        var newY = d3.event.transform.rescaleY(y);
-
-        // update axes with these new boundaries
-        xAxis.call(d3.axisBottom(newX))
-        yAxis.call(d3.axisLeft(newY))
-
-        // update circle position
-        scatter
-            .selectAll("circle")
-            .attr('cx', function (d) { return newX(d["0"]) })
-            .attr('cy', function (d) { return newY(d["1"]) });
-
-        scatter
-            .selectAll("text")
-            .attr('x', function (d) { return newX(d["0"]) })
-            .attr('y', function (d) { return newY(d["1"]) })
-            .style("display", display_text);
-
-
-    }
-
-    //transitions
-
-    function handleMouseOver(d, i) {
-        d3.select(this).transition()
-            .duration(1)
-            .attr("r", 12);
-    }
-
-    function handleMouseOut(d, i) {
-        d3.select(this).transition()
-            .duration(1)
-            .attr("r", 7.5);
-    }
+function handleMouseOut(d, i) {
+    d3.select(this).transition()
+        .duration(1)
+        .attr("r", 7.5);
 }
