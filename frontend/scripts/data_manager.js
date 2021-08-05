0a0fdba6f42a53d9c61d953f02337983f0197edb
Florian Kowarsch <florian.kowarsch@tuwien.ac.at>
2021-08-05 17:53:22 +0200

    added data cache

--- frontend/scripts/data_manager.js
+++ frontend/scripts/data_manager.js
@@ -1,6 +1,16 @@
 var removed_features = []
+var data_dict = {}
 
-function get_embedded_data() {
+async function get_embedded_data() {
+    selection_key = removed_features.join("-")
+
+    if (selection_key in data_dict) {
+        return data_dict[selection_key]
+    } else {
+        data = await embedd_data("../tracks.csv", "../playlists.csv", removed_features);
+        data_dict[selection_key] = data
+        return data
+    }
 
 }
 
@@ -11,6 +21,15 @@ function change_selected_features(feature_name) {
     } else {
         removed_features.push(feature_name)
     }
+    removed_features = removed_features.sort()
 
-    load_data(removed_features)
+    load_data()
+}
+
+
+function load_data() {
+    get_embedded_data().then((data) => {
+        console.log(data)
+        plot_data(data)
+    })
 }
